0bb6c2ca1297dd3e815045885926df1f83faefd4
shyama <skalandoor@paloaltonetworks.com>
2023-02-12 07:38:53 -0800

    ec2

---
+++ ec2.tf
@@ -0,0 +1,63 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_vpc" "example" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "example-vpc"
+  }
+}
+
+resource "aws_subnet" "example" {
+  vpc_id     = aws_vpc.example.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "example-subnet"
+  }
+}
+
+resource "aws_internet_gateway" "example" {
+  vpc_id = aws_vpc.example.id
+
+  tags = {
+    Name = "example-internet-gateway"
+  }
+}
+
+resource "aws_security_group" "example" {
+  name        = "example-security-group"
+  description = "Allow SSH access"
+  vpc_id      = aws_vpc.example.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "example-security-group"
+  }
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.example.id
+
+  tags = {
+    Name = "example-instance"
+  }
+
+  vpc_security_group_ids = [aws_security_group.example.id]
+
+  connection {
+    type        = "ssh"
+    user        = "ec2-user"
+    private_key = file("~/.ssh/id_rsa")
+  }
+}
